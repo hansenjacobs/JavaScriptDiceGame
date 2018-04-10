0ab424b5869c991e087e8a5497528c442184f3ae
Jacob Hansen <hansenjacobs@gmail.com>
2018-04-10 13:41:12 -0500

    intial file creation

---
+++ index.js
@@ -0,0 +1,18 @@
+function playMash(){
+	
+	// Intialize Categories
+	let homeTypes = ['Mansion', 'Apartment', 'Shack', 'House'];
+	// Get user input - Type of Cars (5)
+	// Get user input - Potential Spouses (6)
+	// Get user input - Cities (7)
+	// Get user input - Occupations (8)
+	// Get user input - Children (9)
+
+	//Determine homeType - random num
+
+	//Determine cars - random num
+}
+
+function randomInt(minimumNum, maximumNum){
+	return Math.floor(Math.random() * (maximumNum - minimumNum + 1)) + minimumNum
+}
